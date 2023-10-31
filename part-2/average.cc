8a88f7b80342367912cb96e58f6a38dbd889a4e5
Evan Thompson <evanthompson@csu.fullerton.edu>
2023-10-30 19:40:47 -0700

    finish

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Evan Thompson
+// evanthompson@csu.fullerton.edu
+// @evanthompson86
+// Partners: @gabyvargasgv
 
 #include <iostream>
 #include <string>
@@ -7,10 +10,14 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // TODO: Validate that there is at least one command line argument.
+  // Validate that there is at least one command line argument.
   // If not, print an error message and return a non-zero value.
+  if (arguments.size() < 2) {
+    std::cout << "error: you must supply at least one number\n";
+    return 1;
+  }
 
-  // TODO: Write a for-each loop to sum (add up) all of the command line
+  // Write a for-each loop to sum (add up) all of the command line
   // arguments.
   // Use a double or float type so that your program preserves fractional
   // values.
@@ -19,13 +26,28 @@ int main(int argc, char* argv[]) {
   // Each argument is a std::string. You will need to convert each string into
   // a number with the std::stod or std::stof function.
 
-  // TODO: After the loop has finished summing the arguments, calculate the
+  bool check = true;
+  double total{0.0};
+  for (std::string& argument : arguments) {
+    if (check == true) {
+      check = false;
+      continue;
+    } else {
+      double number{std::stod(argument)};
+      total += number;
+    }
+  }
+
+  // After the loop has finished summing the arguments, calculate the
   // average of the values. Recall that the average is the total value divided
   // by the number of values.
+  double sizes{static_cast<double>(arguments.size() - 1)};
+  double average{total / sizes};
 
-  // TODO: Use cout to print out a message of the form
+  // Use cout to print out a message of the form
   // average = *AVERAGE*
   // on its own line.
+  std::cout << "average = " << average << "\n";
 
   return 0;
 }
